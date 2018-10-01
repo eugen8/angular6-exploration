367c61d32070038a184ad2473e2501a0efd14519
eugen8 <eugen8@gmail.com>
2018-09-30 22:38:24 -0500

    some changes initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-tidier';
+  title = 'Tidier Admin';
+
+
+  constructor() {
+    // console.log(this.title);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CleanerComponent } from './cleaner/cleaner.component';
+import {FormsModule} from '@angular/forms';
+import { CleanerDetailComponent } from './cleaner-detail/cleaner-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CleanerComponent,
+    CleanerDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
